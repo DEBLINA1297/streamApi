24a16e86605d845b3af138fffa4e525515fa757d
DEBLINA1297 <deblina.bhunia@nitsolution.in>
2025-04-17 16:02:52 +0530

    testcommit

--- APRIL16/Question28.java
+++ APRIL16/Question28.java
@@ -2,10 +2,14 @@ package APRIL16;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //Convert a list of strings to a list of their lengths.
 public class Question28 {
     public static void main(String[] args) {
         List<String> l= Arrays.asList("AAA","FGHFH","Deblina");
+        List<Integer> list=l.stream()
+                .map(s->s.length()).collect(Collectors.toList());
+        System.out.println(list);
     }
 }
